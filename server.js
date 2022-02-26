1f707a7d726dd5411614750b73676b9b30e9f1c4
Minh Tien <phamtanminhtien@gmail.com>
2022-02-26 18:15:45 +0700

    --

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/", express.static(path.join(__dirname, "client")));
+app.use("", express.static(path.join(__dirname, "client")));
 
 app.use("/api/link", link);
 app.get("/:id", async (req, res) => {
